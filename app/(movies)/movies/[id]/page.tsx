92587d570bb47bd504bb15ca48033b18005b5f3f
squareCaaat <140076739+squareCaaat@users.noreply.github.com>
2024-08-19 15:12:34 +0900

    Feat dynamic metadata in movie info page

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,17 +1,22 @@
 import { Metadata } from "next";
 import MovieVideos from "../../../../components/movie-videos";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import { Suspense } from "react";
 
-export const metadata: Metadata = {
-  title: "Movie",
+interface IParams {
+  params: {id: string};
 };
+
+export async function generateMetadata({params: {id}}:IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
 
 export default async function MovieDetail({
   params: { id },
-}: {
-  params: { id: string };
-}) {
+}: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading Movie Info</h1>}>
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,7 +1,7 @@
 import { API_URL } from "../app/(home)/page";
 import styles from '../styles/movie-info.module.css';
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
     // await new Promise((resolve) => setTimeout(resolve, 1000));
     const response = await fetch(`${API_URL}/${id}`);
     return response.json();
@@ -14,7 +14,7 @@ export default async function MovieInfo({id}:{id:string}) {
             <img className={styles.poster} src={movie.poster_path} />
             <div className={styles.info}>
                 <h1 className={styles.title}>{movie.title}</h1>
-                <h3>⭐ {movie.vote_average.toFixed(1)} ({movie.vote_count} voted)</h3>
+                <h3>⭐ {movie.vote_average.toFixed(1)} ({movie.vote_count} people voted)</h3>
                 <p>{movie.overview}</p>
                 <a href={movie.homepage} target={"_blank"}>Homepage &rarr;</a>
             </div>
