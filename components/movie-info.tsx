6eefa7d643fd50c366aedcdbfa747cabb7687788
squareCaaat <140076739+squareCaaat@users.noreply.github.com>
2024-08-19 14:18:36 +0900

    Add movie info css

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import styles from '../styles/movie-info.module.css';
 
 async function getMovie(id: string) {
     // await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -8,5 +9,15 @@ async function getMovie(id: string) {
 
 export default async function MovieInfo({id}:{id:string}) {
     const movie = await getMovie(id);
-    return (<h6>{JSON.stringify(movie)}</h6>);
+    return (
+        <div className={styles.container}>
+            <img className={styles.poster} src={movie.poster_path} />
+            <div className={styles.info}>
+                <h1 className={styles.title}>{movie.title}</h1>
+                <h3>⭐ {movie.vote_average.toFixed(1)} ({movie.vote_count} voted)</h3>
+                <p>{movie.overview}</p>
+                <a href={movie.homepage} target={"_blank"}>Homepage &rarr;</a>
+            </div>
+        </div>
+    );
 }
